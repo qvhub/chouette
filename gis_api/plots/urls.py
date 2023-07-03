4b225539749affb891b5fff141ecbb12ff33853d
Quentin <contact@quentinvautier.com>
2023-07-03 22:47:00 +0200

    first commit

--- gis_api/plots/urls.py
+++ gis_api/plots/urls.py
@@ -3,7 +3,7 @@ from .views import ParcelCreateView, ParcelRetrieveUpdateView, UserParcelsListVi
 
 urlpatterns = [
     path('create/', ParcelCreateView.as_view(), name='create_parcel'),
-    path('parcel/<int:pk>/', ParcelRetrieveUpdateView.as_view(), name='parcel-update'),
-    path('parcel/<int:pk>/delete/', ParcelDeleteView.as_view(), name='parcel-delete'),
-    path('user-parcels/', UserParcelsListView.as_view(), name='user-parcels-list'),
+    path('<int:pk>/', ParcelRetrieveUpdateView.as_view(), name='parcel-update'),
+    path('<int:pk>/delete/', ParcelDeleteView.as_view(), name='parcel-delete'),
+    path('user/', UserParcelsListView.as_view(), name='user-parcels-list'),
 ]
